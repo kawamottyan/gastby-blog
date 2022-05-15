cde18649e515cd7f4cac3984f07d116d97e5e02e
Masato Kawamoto <kawamotomasato.private@gmail.com>
2022-05-15 16:08:30 +0900

    change footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,7 +7,8 @@ const Footer = () => (
   <Container as="footer">
     <div className={styles.container}>
       Follow me on <a href="https://twitter.com/kawa_mottyan" target="_blank">Twitter</a> and{' '}
-      <a href="https://www.facebook.com/kawamottyan" target="_blank">Facebook</a> &middot;{' '}
+      <a href="https://www.facebook.com/kawamottyan" target="_blank">Facebook</a> {' '}!
+      <br></br>
       <a href="https://github.com/kawamottyan/gastby-blog" target="_blank">Report issues</a>
     </div>
   </Container>
